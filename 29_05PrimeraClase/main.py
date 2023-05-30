a680116242634e031f88105682bca7b99861a8b9
JoakoNietoP <joaquin.nietop@outlook.com>
2023-05-30 00:12:29 -0300

    pruebas con print

--- 29_05PrimeraClase/main.py
+++ 29_05PrimeraClase/main.py
@@ -1,1 +1,11 @@
 print("hello world")
+
+mensaje = "Quiero comer un helado"
+print(mensaje)
+
+mensaje2 = "Vayamos a la heladeria"
+
+print(mensaje, mensaje2)
+
+nuevo_msg = mensaje + " " + "Deberiamos ir por un cono"
+print(nuevo_msg)
