aee5c89f17a3d942a091035fcb68242e71011f77
JoakoNietoP <joaquin.nietop@outlook.com>
2023-05-30 05:42:26 -0300

    Configurando los entornos

--- 29_05PrimeraClase/main.py
+++ 29_05PrimeraClase/main.py
@@ -3,9 +3,11 @@ print("hello world")
 mensaje = "Quiero comer un helado"
 print(mensaje)
 
-mensaje2 = "Vayamos a la heladeria"
+mensaje_2 = "¡Vayamos a la heladeria"
 
-print(mensaje, mensaje2)
+print(mensaje, mensaje_2)
 
 nuevo_msg = mensaje + " " + "Deberiamos ir por un cono"
 print(nuevo_msg)
+
+#! seleccionando codigo y luego shift + enter te abre la terminal de python
