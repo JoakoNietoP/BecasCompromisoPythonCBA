6b98d12ca6cd536298014fac9f0ee76ef78d57cd
JoakoNietoP <joaquin.nietop@outlook.com>
2023-05-31 01:46:30 -0300

    pruebas con input

--- 29_05PrimeraClase/main.py
+++ 29_05PrimeraClase/main.py
@@ -10,4 +10,7 @@ print(mensaje, mensaje_2)
 nuevo_msg = mensaje + " " + "Deberiamos ir por un cono"
 print(nuevo_msg)
 
+nombre = input("Ingresa tu nombre: ")
+print("Hola,", nombre, "eres un crack")
+
 #! seleccionando codigo y luego shift + enter te abre la terminal de python
